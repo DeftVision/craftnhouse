b51fa97f4a1ba571f43f70ef7b9eb25c34dbc6f6
Gabriel <gjbarret@asu.edu>
2025-06-02 12:04:37 -0600

    Added content to pages for animation testing

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,17 +4,45 @@ import { Box, Typography } from '@mui/material';
 
 export default function About () {
     return (
-        <Box
-            sx={{
-                maxWidth: '600px',
-                mx: 'auto',
-                py: 4,
-                textAlign: 'center'
-            }}
+       <>
+           <Box
+               sx={{
+                   maxWidth: '600px',
+                   mx: 'auto',
+                   py: 4,
+                   textAlign: 'center'
+               }}
 
-        >
-            <Typography variant='h4'>About</Typography>
-            <Typography variant='h5'>This is your about page content.</Typography>
-        </Box>
+           >
+               <Typography variant='h4'>About</Typography>
+               <Typography variant='h5'>This is your about page content.</Typography>
+           </Box>
+
+           <Box
+               sx={{
+                   maxWidth: '600px',
+                   mx: 'auto',
+                   py: 4,
+                   textAlign: 'center'
+               }}
+
+           >
+               <Typography variant='h4'>About</Typography>
+               <Typography variant='h5'>Section 2</Typography>
+           </Box>
+
+           <Box
+               sx={{
+                   maxWidth: '600px',
+                   mx: 'auto',
+                   py: 4,
+                   textAlign: 'center'
+               }}
+
+           >
+               <Typography variant='h4'>About</Typography>
+               <Typography variant='h5'>Section 3</Typography>
+           </Box>
+       </>
     );
 };
