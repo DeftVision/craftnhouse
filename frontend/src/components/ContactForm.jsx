6659c532db3959709df2c25461c4c01a408ee121
Gabriel <gabriel@deftvision.com>
2025-06-02 04:58:48 -0600

    Create contact form

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import  { Box } from '@mui/material'
+import React, { useState } from 'react';
+import {Box, Typography, TextField, Button, Stack} from '@mui/material'
 
 export default function ContactForm () {
-    return (
-        <Box>
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(false);
 
+    return (
+        <Box component='form'>
+            <Typography variant='h4'>Contact Us</Typography>
+            <Stack direction='column' spacing={2}>
+                <TextField
+                    type='text'
+                    label='Name'
+                    fullWidth
+                    name='name'
+                    required
+                    arial-label='Name'
+                />
+                <TextField
+                    type='email'
+                    label='Email'
+                    fullWidth
+                    name='email'
+                    required
+                    arial-label='Email'
+                />
+                <TextField
+                    type='text'
+                    multiline
+                    label='Message'
+                    fullWidth
+                    name='message'
+                    required
+                    arial-label='Message'
+                />
+            </Stack>
+            <Button type='submit' variant='contained'>Submit</Button>
         </Box>
     );
 };
