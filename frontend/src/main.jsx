7ef53a3d27ee197d006f1867d08178772e1684af
Gabriel <gjbarret@asu.edu>
2025-06-03 14:41:32 -0600

    Wrapped helmet provider around app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,16 @@ import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter as Router} from 'react-router-dom'
 import MuiThemeProvider from './theme/MuiThemeProvider';
-
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Router>
-            <MuiThemeProvider>
-                <App/>
-            </MuiThemeProvider>
-        </Router>
+        <HelmetProvider>
+            <Router>
+                <MuiThemeProvider>
+                    <App/>
+                </MuiThemeProvider>
+            </Router>
+        </HelmetProvider>
     </StrictMode>,
 )
